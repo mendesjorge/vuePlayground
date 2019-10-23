314cba416678502ef1e27a910fe15c25fb2361e0
mendesjorge <luis.7win@gmail.com>
2019-10-23 23:28:25 +0100

    finished table base

---
+++ src/components/Table.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="table">
+    Aqui vai aparecer a tabela
+    <div class="table-wrapper">
+      <div class="table-header">
+        <div class="header-cell">Nome</div>
+        <div class="header-cell">Idade</div>
+        <div class="header-cell">Sexo</div>
+        <div class="header-cell">Morada</div>
+        <div class="header-cell">Estado</div>
+      </div>
+      <div class="table-body">
+        <div class="table-row" v-for="user in users">
+          <div class="header-cell">{{user.Nome}}</div>
+          <div class="header-cell">{{user.Idade}}</div>
+          <div class="header-cell">{{user.Sexo}}</div>
+          <div class="header-cell">{{user.Morada}}</div>
+          <div class="header-cell">{{user.Estado}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      users: [
+        {
+          Nome: "Jose",
+          Idade: 31,
+          Sexo: "Masculino",
+          Morada: "Na esquina",
+          Estado: "Activo"
+        },
+        {
+          Nome: "Maria",
+          Idade: 24,
+          Sexo: "Feminino",
+          Morada: "Na esquina",
+          Estado: "Activo"
+        },
+        {
+          Nome: "Antonia",
+          Idade: 60,
+          Sexo: "Feminino",
+          Morada: "Na esquina",
+          Estado: "Activo"
+        }
+      ]
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.table-wrapper{
+
+  .table-header{
+    display: flex;
+    background-color: lightblue;
+
+    .header-cell{
+      width: calc(100% / 5);
+      padding:10px 15px;
+    }
+  }
+
+  .table-body{
+    .table-row{
+      display: flex;
+      background-color:lightcoral;
+
+      .header-cell{
+        width: calc(100% / 5);
+        padding:10px 15px;
+
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Table.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="table">
+    <app-table/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Table from '@/components/Table.vue'
+
+export default {
+  components: {
+    "app-table": Table
+  }
+}
+</script>
