661529b882f4c92f0f3589a35384442df1493351
mendesjorge <luis.7win@gmail.com>
2019-10-29 23:16:55 +0000

    cant access props in computed properties, need to check

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -4,12 +4,12 @@
     <div class="table-wrapper">
       <div class="table-header">
         <div
-          v-for="config in configs"
-          :key="config.name"
+          v-for="column in columnsConfiguration"
+          :key="column.name"
           class="header-cell"
-          :style="{'width':`${config.width}%`}"
+          :style="{'width':`${column.width}%`}"
         >
-          {{config.display}}
+          {{column.display}}
         </div>
       </div>
       <div class="table-body">
@@ -31,70 +31,38 @@ import TableRow from './TableRow.vue'
 export default {
   components: {'app-table-row': TableRow},
   props: {
-    msg: String
+    configurations: Array,
+    data: Array
   },
   data(){
     return {
-      users: [
-        {
-          Nome: "Jose",
-          Idade: 31,
-          Sexo: "Masculino",
-          Morada: "Na esquina",
-          Estado: true
-        },
-        {
-          Nome: "Maria",
-          Idade: 24,
-          Sexo: "Feminino",
-          Morada: "Na esquina",
-          Estado: true
-        },
-        {
-          Nome: "Antonia",
-          Idade: 60,
-          Sexo: "Feminino",
-          Morada: "Na esquina",
-          Estado: false
-        }
-      ],
-      columns: [
-        {attribute: 'Nome', display: 'NAME', type: 'text', columnWeight: 3},
-        {attribute: 'Idade', display: 'IDADE', type: 'text'},
-        {attribute: 'Sexo', display: 'SEXO', type: 'text'},
-        {attribute: 'Morada', display: 'MORADA', type: 'text',columnWeight: 6},
-        {attribute: 'Estado', display: 'ESTADO', type: 'switch', valueMap: value => value? 'Activo':'Inactivo'}
-      ]
-    };
+    }
   },
   computed: {
-    configs() {
+    columnsConfiguration() {
+      console.log(this)
       // math to get table column width
-      let columnWidth = 100 / this.columns.reduce((prev, data) => prev + (data.columnWeight || 1), 0)
+      let columnWidth = 100 / this.configurations.reduce((prev, data) => prev + (data.columnWeight || 1), 0)
 
-      let ret =  this.columns.map(column => (
+      return this.configurations.map(column => (
         {
           ...column,
           width: (columnWidth * (column.columnWeight || 1)).toFixed(2)
         }
       ))
-      console.log(ret)
-      return ret
     },
     rows() {
-      let rows = this.users.map( (row, index) => {
-
-        return this.configs.map(config => (
+      return this.data.map( (row, index) => {
+        return this.columnsConfiguration.map(column => (
           {
-            uid:          `${config.attribute}-${index}`,
-            value:        row[config.attribute],
-            display:      config.valueMap && config.valueMap(row[config.attribute]),
-            type:         config.type,
-            width:        config.width
+            uid:          `${column.attribute}-${index}`,
+            value:        row[column.attribute],
+            display:      column.valueMap && column.valueMap(row[column.attribute]),
+            type:         column.type,
+            width:        column.width
           }
         ))
       })
-      return rows
     }
   }
 }
--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="table">
+  <div class="table"
+    :configurations="configurations"
+    :data="users"
+  >
     <app-table/>
   </div>
 </template>
@@ -11,6 +14,40 @@ import Table from '@/components/Table.vue'
 export default {
   components: {
     "app-table": Table
+  },
+  data(){
+    return {
+      users: [
+        {
+          Nome: "Jose",
+          Idade: 31,
+          Sexo: "Masculino",
+          Morada: "Na esquina",
+          Estado: true
+        },
+        {
+          Nome: "Maria",
+          Idade: 24,
+          Sexo: "Feminino",
+          Morada: "Na esquina",
+          Estado: true
+        },
+        {
+          Nome: "Antonia",
+          Idade: 60,
+          Sexo: "Feminino",
+          Morada: "Na esquina",
+          Estado: false
+        }
+      ],
+      configurations: [
+        {attribute: 'Nome', display: 'NAME', type: 'text', columnWeight: 3},
+        {attribute: 'Idade', display: 'IDADE', type: 'text'},
+        {attribute: 'Sexo', display: 'SEXO', type: 'text'},
+        {attribute: 'Morada', display: 'MORADA', type: 'text',columnWeight: 6},
+        {attribute: 'Estado', display: 'ESTADO', type: 'switch', valueMap: value => value? 'Activo':'Inactivo'}
+      ]
+    }
   }
 }
 </script>
