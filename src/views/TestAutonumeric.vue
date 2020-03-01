e57d57e18290da2511e31a7ad784374b6c006200
mendesjorge <luis.7win@gmail.com>
2020-03-01 15:25:34 +0000

    added vue moment and autonumeric test component

---
+++ src/views/TestAutonumeric.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="">
+		<p>
+			moment in action
+		</p> 
+		{{date | moment('DD MMM YYYY')}}
+	</div>
+</template>
+<script>
+	export default{
+		data(){
+			return {
+				date: new Date()
+			}
+		},
+		beforeCreate(){
+			this.$moment.locale('pt', {
+				monthsShort:['JAN', 'FEV','MAR','ABR','MAI','JUN','JUL','AGO','SET','OUT','NOV','DEZ']
+			})
+		}
+	}
+</script>
+<styles>
+	
+</styles>
